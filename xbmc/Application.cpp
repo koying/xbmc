1e742c9267cab74e9257c834ed30ba5c09af37bd
Chris "Koying" Browet <cbro@semperpax.com>
2016-12-03 13:44:01 +0100

    CHG: [droid] log lib paths at startup

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -191,6 +191,8 @@
 
 #if defined(TARGET_ANDROID)
 #include <androidjni/Build.h>
+#include <androidjni/ApplicationInfo.h>
+#include <androidjni/System.h>
 #include "platform/android/activity/XBMCApp.h"
 #include "platform/android/activity/AndroidFeatures.h"
 #endif
@@ -523,6 +525,9 @@ bool CApplication::Create(const CAppParamParser &params)
   std::string extstorage;
   bool extready = CXBMCApp::GetExternalStorage(extstorage);
   CLog::Log(LOGNOTICE, "External storage path = %s; status = %s", extstorage.c_str(), extready ? "ok" : "nok");
+  CLog::Log(LOGNOTICE, "System library paths = %s", CJNISystem::getProperty("java.library.path").c_str());
+  CLog::Log(LOGNOTICE, "App library path = %s", CXBMCApp::getApplicationInfo().nativeLibraryDir.c_str());
+  CLog::Log(LOGNOTICE, "APK = %s", CXBMCApp::getPackageResourcePath().c_str());
 #endif
 
 #if defined(__arm__) || defined(__aarch64__)
