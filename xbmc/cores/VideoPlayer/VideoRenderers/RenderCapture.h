koying/SPMC

ADD: [droid] Screen capture via Media Projection
Allows to capture the Surface View bits

Issues:
- Android requires runtime confirmation from user
- Although the dialog is discardable, SPMC still looses the focus on
  subsequent calls -> video is paused/stopped

Mitigation:
Confirmation is asked on startup and false-defaulted advanced setting

xbmc/cores/VideoRenderers/RenderCapture.h
@@ -172,8 +172,31 @@ class CRenderCaptureBase
     bool             m_asyncChecked;
 };
 
+#if defined(TARGET_ANDROID)
+#include "system_gl.h"
 
-#if defined(HAS_IMXVPU)
+class CRenderCaptureDroid : public CRenderCaptureBase
+{
+public:
+  CRenderCaptureDroid();
+  ~CRenderCaptureDroid();
+
+  int   GetCaptureFormat() { return CAPTUREFORMAT_BGRA; }
+
+  void  BeginRender();
+  void  EndRender();
+  void  ReadOut();
+
+  void* GetRenderBuffer();
+};
+
+class CRenderCapture : public CRenderCaptureDroid
+{
+public:
+  CRenderCapture() {}
+};
+
+#elif defined(HAS_IMXVPU)
 #include "../dvdplayer/DVDCodecs/Video/DVDVideoCodecIMX.h"
 
 class CRenderCaptureIMX : public CRenderCaptureBase

koying/xbmc

xbmc/cores/VideoPlayer/VideoRenderers/RenderCapture.h
@@ -112,7 +112,31 @@ class CRenderCaptureBase
     bool m_asyncChecked;
 };
 
-#if defined(TARGET_RASPBERRY_PI)
+#if defined(TARGET_ANDROID)
+#include "system_gl.h"
+
+class CRenderCaptureDroid : public CRenderCaptureBase
+{
+public:
+  CRenderCaptureDroid();
+  ~CRenderCaptureDroid();
+
+  int   GetCaptureFormat();
+
+  void  BeginRender();
+  void  EndRender();
+  void  ReadOut();
+
+  void* GetRenderBuffer();
+};
+
+class CRenderCapture : public CRenderCaptureDroid
+{
+public:
+  CRenderCapture() {}
+};
+
+#elif defined(TARGET_RASPBERRY_PI)
 #include "platform/linux/RBP.h"
 
 class CRenderCaptureDispmanX : public CRenderCaptureBase