koying/SPMC

ADD: [droid] Screen capture via Media Projection
Allows to capture the Surface View bits

Issues:
- Android requires runtime confirmation from user
- Although the dialog is discardable, SPMC still looses the focus on
  subsequent calls -> video is paused/stopped

Mitigation:
Confirmation is asked on startup and false-defaulted advanced setting

xbmc/cores/VideoRenderers/RenderCapture.cpp
@@ -23,6 +23,13 @@
 #include "windowing/WindowingFactory.h"
 #include "settings/AdvancedSettings.h"
 
+#if defined(TARGET_ANDROID)
+#include "android/activity/XBMCApp.h"
+extern "C" {
+#include "libswscale/swscale.h"
+}
+#endif
+
 CRenderCaptureBase::CRenderCaptureBase()
 {
   m_state          = CAPTURESTATE_FAILED;
@@ -52,8 +59,93 @@ bool CRenderCaptureBase::UseOcclusionQuery()
     return true;
 }
 
+#if defined(TARGET_ANDROID)
+CRenderCaptureDroid::CRenderCaptureDroid()
+{
+}
 
-#if defined(HAS_IMXVPU)
+CRenderCaptureDroid::~CRenderCaptureDroid()
+{
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+    CXBMCApp::StopCapture();
+  delete[] m_pixels;
+}
+
+void CRenderCaptureDroid::BeginRender()
+{
+  m_asyncChecked = true;
+
+  if (m_bufferSize != m_width * m_height * 4)
+  {
+    delete[] m_pixels;
+    m_bufferSize = m_width * m_height * 4;
+    m_pixels = new uint8_t[m_bufferSize];
+  }
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+  {
+    m_asyncSupported = true;
+    CXBMCApp::startCapture(m_width, m_height);
+  }
+}
+
+void CRenderCaptureDroid::EndRender()
+{
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+  {
+    if (m_flags & CAPTUREFLAG_IMMEDIATELY)
+      ReadOut();
+    else
+      SetState(CAPTURESTATE_NEEDSREADOUT);
+  }
+  else
+    SetState(CAPTURESTATE_DONE);
+}
+
+void* CRenderCaptureDroid::GetRenderBuffer()
+{
+    return m_pixels;
+}
+
+void CRenderCaptureDroid::ReadOut()
+{
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+  {
+    jni::CJNIImage image;
+    if (CXBMCApp::GetCapture(image))
+    {
+      int iWidth = image.getWidth();
+      int iHeight = image.getHeight();
+
+      std::vector<jni::CJNIImagePlane> planes = image.getPlanes();
+      CJNIByteBuffer bytebuffer = planes[0].getBuffer();
+
+      struct SwsContext *context = sws_getContext(iWidth, iHeight, PIX_FMT_RGBA,
+                                                  m_width, m_height, PIX_FMT_BGRA,
+                                                  SWS_FAST_BILINEAR, NULL, NULL, NULL);
+
+      void *buf_ptr = xbmc_jnienv()->GetDirectBufferAddress(bytebuffer.get_raw());
+
+      uint8_t *src[] = { (uint8_t*)buf_ptr, 0, 0, 0 };
+      int     srcStride[] = { planes[0].getRowStride(), 0, 0, 0 };
+
+      uint8_t *dst[] = { m_pixels, 0, 0, 0 };
+      int     dstStride[] = { (int)m_width * 4, 0, 0, 0 };
+
+      if (context)
+      {
+        sws_scale(context, src, srcStride, 0, iHeight, dst, dstStride);
+        sws_freeContext(context);
+      }
+
+      image.close();
+      SetState(CAPTURESTATE_DONE);
+    }
+    else
+      SetState(CAPTURESTATE_FAILED);
+  }
+}
+
+#elif defined(HAS_IMXVPU)
 CRenderCaptureIMX::CRenderCaptureIMX()
 {
 }

xbmc/cores/VideoRenderers/RenderCapture.h
@@ -172,8 +172,31 @@ class CRenderCaptureBase
     bool             m_asyncChecked;
 };
 
+#if defined(TARGET_ANDROID)
+#include "system_gl.h"
 
-#if defined(HAS_IMXVPU)
+class CRenderCaptureDroid : public CRenderCaptureBase
+{
+public:
+  CRenderCaptureDroid();
+  ~CRenderCaptureDroid();
+
+  int   GetCaptureFormat() { return CAPTUREFORMAT_BGRA; }
+
+  void  BeginRender();
+  void  EndRender();
+  void  ReadOut();
+
+  void* GetRenderBuffer();
+};
+
+class CRenderCapture : public CRenderCaptureDroid
+{
+public:
+  CRenderCapture() {}
+};
+
+#elif defined(HAS_IMXVPU)
 #include "../dvdplayer/DVDCodecs/Video/DVDVideoCodecIMX.h"
 
 class CRenderCaptureIMX : public CRenderCaptureBase

koying/xbmc

xbmc/cores/VideoPlayer/VideoRenderers/RenderCapture.cpp
@@ -34,6 +34,13 @@ extern "C" {
 #include "libavutil/mem.h"
 }
 
+#if defined(TARGET_ANDROID)
+#include "platform/android/activity/XBMCApp.h"
+extern "C" {
+#include "libswscale/swscale.h"
+}
+#endif
+
 CRenderCaptureBase::CRenderCaptureBase()
 {
   m_state          = CAPTURESTATE_FAILED;
@@ -61,7 +68,98 @@ bool CRenderCaptureBase::UseOcclusionQuery()
     return true;
 }
 
-#if defined(TARGET_RASPBERRY_PI)
+#if defined(TARGET_ANDROID)
+CRenderCaptureDroid::CRenderCaptureDroid()
+{
+}
+
+CRenderCaptureDroid::~CRenderCaptureDroid()
+{
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+    CXBMCApp::StopCapture();
+  delete[] m_pixels;
+}
+
+int CRenderCaptureDroid::GetCaptureFormat()
+{
+  return CAPTUREFORMAT_BGRA;
+}
+
+void CRenderCaptureDroid::BeginRender()
+{
+  m_asyncChecked = true;
+
+  if (m_bufferSize != m_width * m_height * 4)
+  {
+    delete[] m_pixels;
+    m_bufferSize = m_width * m_height * 4;
+    m_pixels = new uint8_t[m_bufferSize];
+  }
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+  {
+    m_asyncSupported = true;
+    CXBMCApp::startCapture(m_width, m_height);
+  }
+}
+
+void CRenderCaptureDroid::EndRender()
+{
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+  {
+    if (m_flags & CAPTUREFLAG_IMMEDIATELY)
+      ReadOut();
+    else
+      SetState(CAPTURESTATE_NEEDSREADOUT);
+  }
+  else
+    SetState(CAPTURESTATE_DONE);
+}
+
+void* CRenderCaptureDroid::GetRenderBuffer()
+{
+    return m_pixels;
+}
+
+void CRenderCaptureDroid::ReadOut()
+{
+  if (g_advancedSettings.m_videoUseDroidProjectionCapture)
+  {
+    jni::CJNIImage image;
+    if (CXBMCApp::GetCapture(image))
+    {
+      int iWidth = image.getWidth();
+      int iHeight = image.getHeight();
+
+      std::vector<jni::CJNIImagePlane> planes = image.getPlanes();
+      CJNIByteBuffer bytebuffer = planes[0].getBuffer();
+
+      struct SwsContext *context = sws_getContext(iWidth, iHeight, AV_PIX_FMT_BGRA,
+                                                  m_width, m_height, AV_PIX_FMT_BGRA,
+                                                  SWS_FAST_BILINEAR, NULL, NULL, NULL);
+
+      void *buf_ptr = xbmc_jnienv()->GetDirectBufferAddress(bytebuffer.get_raw());
+
+      uint8_t *src[] = { (uint8_t*)buf_ptr, 0, 0, 0 };
+      int     srcStride[] = { planes[0].getRowStride(), 0, 0, 0 };
+
+      uint8_t *dst[] = { m_pixels, 0, 0, 0 };
+      int     dstStride[] = { (int)m_width * 4, 0, 0, 0 };
+
+      if (context)
+      {
+        sws_scale(context, src, srcStride, 0, iHeight, dst, dstStride);
+        sws_freeContext(context);
+      }
+
+      image.close();
+      SetState(CAPTURESTATE_DONE);
+    }
+    else
+      SetState(CAPTURESTATE_FAILED);
+  }
+}
+
+#elif defined(TARGET_RASPBERRY_PI)
 
 CRenderCaptureDispmanX::CRenderCaptureDispmanX()
 {

xbmc/cores/VideoPlayer/VideoRenderers/RenderCapture.h
@@ -111,7 +111,31 @@ class CRenderCaptureBase
     bool m_asyncChecked;
 };
 
-#if defined(TARGET_RASPBERRY_PI)
+#if defined(TARGET_ANDROID)
+#include "system_gl.h"
+
+class CRenderCaptureDroid : public CRenderCaptureBase
+{
+public:
+  CRenderCaptureDroid();
+  ~CRenderCaptureDroid();
+
+  int   GetCaptureFormat();
+
+  void  BeginRender();
+  void  EndRender();
+  void  ReadOut();
+
+  void* GetRenderBuffer();
+};
+
+class CRenderCapture : public CRenderCaptureDroid
+{
+public:
+  CRenderCapture() {}
+};
+
+#elif defined(TARGET_RASPBERRY_PI)
 #include "platform/linux/RBP.h"
 
 class CRenderCaptureDispmanX : public CRenderCaptureBase